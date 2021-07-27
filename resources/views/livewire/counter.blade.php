638fa34c911f2b7f44314a3f3aa23f729bb5c578
Atiqur Rahman <ar.diu.cse@gmail.com>
2021-07-27 15:39:56 +0600

    Counter app using Livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div class="text-center">
+    <h3 class="text-4xl">Counter Application Using Laravel-Livewire</h3>
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    <button wire:click="decrement">-</button>
+
+</div>
