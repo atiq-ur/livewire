1b26813169193f58e02873389dcee7fe49fc90c0
atiq-ur <ar.diu.cse@gmail.com>
2021-08-07 00:59:42 +0600

    Support Ticket

---
+++ database/factories/SupportTicketFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SupportTicket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+class SupportTicketFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SupportTicket::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'question' => $this->faker->paragraph(),
+        ];
+    }
+}
