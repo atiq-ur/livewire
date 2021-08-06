6668285d9d31acf720d2e11d2964aad78a7c207d
atiq-ur <ar.diu.cse@gmail.com>
2021-08-06 11:12:37 +0600

    livewire Pagination

--- app/Http/Livewire/Comment.php
+++ app/Http/Livewire/Comment.php
@@ -4,15 +4,29 @@ namespace App\Http\Livewire;
 
 use Carbon\Carbon;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Comment extends Component
 {
+    use WithPagination;
     public $newComment;
-    public $comments;
+//    public $comments;
+
+    /**
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName, [
+            'newComment' => 'required|max:255',
+        ]);
+    }
 
     public function addComment()
     {
-        if ($this->newComment == "") return;
+        $this->validate(['newComment' => 'required|max:255']);
+
+        //if ($this->newComment == "") return;
         // array_unshift($this->comments, [
         //     'body' => $this->newComment,
         //     'created_at' => Carbon::now()->diffForHumans(),
@@ -21,17 +35,31 @@ class Comment extends Component
         $newComment = \App\Models\Comment::create(
             ['body' => $this->newComment, 'user_id' => 1]
         );
-        $this->comments->prepend($newComment);
         $this->newComment = "";
+        session()->flash('message','Comment added successfully');
     }
 
     public function mount()
     {
-        $comment = \App\Models\Comment::latest()->get();
-        $this->comments = $comment;
+        /*$comment = \App\Models\Comment::latest()->get();
+        $this->comments = $comment;*/
+    }
+
+    public function remove($commentId)
+    {
+        $comment = \App\Models\Comment::find($commentId);
+        //$this->comments = $this->comments->where('id', '!=', $commentId);
+        $comment->delete();
+//        $this->comments = $this->comments->except($commentId);
+//        dd($comment);
+        //dd($commentId);
+        session()->flash('message','Comment deleted successfully');
     }
     public function render()
     {
-        return view('livewire.comment');
+        return view('livewire.comment',
+        [
+            'comments' => \App\Models\Comment::paginate(2)
+        ]);
     }
 }
--- resources/views/livewire/comment.blade.php
+++ resources/views/livewire/comment.blade.php
@@ -1,11 +1,19 @@
 <div class="flex justify-center">
     <div class="w-6/12">
         <h1 class="my-10 text-3xl">Comment</h1>
+        @error('newComment') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
+        <div>
+            @if(session()->has('message'))
+                <div class="p-3 bg-green-300 text-green-800 rounded shadow">
+                    {{ session('message') }}
+                </div>
+            @endif
+        </div>
         <form class="my-4 flex" wire:submit.prevent="addComment">
             {{-- {{ $newComment }} --}}
             <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2"
                    {{--Lazy used for hitting AJAX req on focus outside--}}
-            placeholder="What's in your mind." wire:model.lazy="newComment">
+            placeholder="What's in your mind." wire:model.debounce.500ms="newComment">
             <div class="py-2">
                 <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">
                     Add
@@ -14,17 +22,22 @@
         </form>
         @foreach ($comments as $comment)
             <div class="rounded border shadow p-3 my-2">
-                <div class="flex justify-start my-2">
-                    <p class="font-bold text-lg">{{ $comment->creator->name }}</p>
-                    <p class="mx-3 py-2 text-xs text-gray-500 font-semibold">
-                        {{--{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}--}}
-                        {{ \Carbon\Carbon::parse($comment->created_at) }}
-                    </p>
+                <div class="flex justify-between my-2">
+                    <div class="flex">
+                        <p class="font-bold text-lg">{{ $comment->creator->name }}</p>
+                        <p class="mx-3 py-2 text-xs text-gray-500 font-semibold">
+                            {{--{{ \Carbon\Carbon::parse($comment->created_at)->diffForHumans() }}--}}
+                            {{ \Carbon\Carbon::parse($comment->created_at) }}
+                        </p>
+                    </div>
+                    <button class="text-xs text-red-500" wire:click="remove({{ $comment->id }})">X</button>
+
                 </div>
                 <p class="text-gray-800">
                     {{ $comment->body }}
                 </p>
         </div>
         @endforeach
+        {{ $comments->links('pagination-links') }}
     </div>
 </div>
