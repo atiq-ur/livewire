9b942c9b856c224e3db3351d5970e9987f7f9390
atiq-ur <ar.diu.cse@gmail.com>
2021-08-07 12:23:40 +0600

    Final

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 
 class Home extends Component
 {
+
     public function render()
     {
         return view('livewire.home');
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@ Route::get('/comments', function () {
     return view('comments');
 });*/
 
-Route::get('/', \App\Http\Livewire\Home::class)->name('home');
-Route::get('/login', \App\Http\Livewire\Login::class)->name('login');
-Route::get('/register', \App\Http\Livewire\Register::class)->name('register');
+Route::get('/', \App\Http\Livewire\Home::class)->name('home')
+    ->middleware('auth');
+
+Route::group(['middleware' => 'guest'], function (){
+    Route::get('/login', \App\Http\Livewire\Login::class)->name('login');
+    Route::get('/register', \App\Http\Livewire\Register::class)->name('register');
+});
+
 
